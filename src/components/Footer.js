07c7211873696295e4702b12f2fea4a5a8c70c67
lmintsys <lubmin23@gmail.com>
2023-05-17 19:50:41 -0400

    added icons to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,33 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faStackOverflow } from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
+  return (
+    <footer
+      className="text-center text-white"
+      style={{ backgroundColor: "#f1f1f1" }}
+      id="footer"
+    >
+      <div>
+        <a href="https://github.com/lmintsys" className="social-media">
+          <FontAwesomeIcon icon={faGithub} size="2xl" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/luba-mintsys/"
+          className="social-media"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+        </a>
+        <a
+          href="https://stackoverflow.com/users/21910018/lumin"
+          className="social-media"
+        >
+          <FontAwesomeIcon icon={faStackOverflow} size="2xl" />
+        </a>
+      </div>
+    </footer>
+  );
 }
