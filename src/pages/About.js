bd321b87702ebbae7b5b9377132fe5c6d65fccc8
lmintsys <lubmin23@gmail.com>
2023-05-17 19:48:52 -0400

    adjusted About and Resume pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,5 +2,19 @@ import React from "react";
 
 export default function About() {
   return (
+    <main>
+      <div className="title container">
+        <h2>About Me</h2>
+      </div>
+      <div className="container">
+        <img src="/assets/images/profile.png" alt="profile" id="profile"></img>
+        <p className="text">
+          Hello! My name is Luba Mintsys. I am coming from a psychology field,
+          but have always been interested in programming. I have been working
+          with C#, HTML, CSS, and JavaScript. I am also experienced with Aodbe
+          Photoshop and Adobe Illustrator.
+        </p>
+      </div>
+    </main>
   );
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,4 +1,46 @@
 import React from "react";
 
 export default function Resume() {
+  const frontEnd = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "JQuery",
+    "Responsive Design",
+    "React",
+    "Bootstrap",
+    "Bulma",
+  ];
+  const backEnd = [
+    "API",
+    "Node",
+    "Express",
+    "MySQL, Sequelize",
+    "MongoDB, Mongoose",
+    "REST",
+    "GraphQL",
+  ];
+  const fronProf = frontEnd.map((prof) => {
+    return <li>{prof}</li>;
+  });
+  const backProf = backEnd.map((prof) => {
+    return <li>{prof}</li>;
+  });
+  return (
+    <div>
+      <div class="title container">
+        <h2>Resume</h2>
+      </div>
+      <div class="container text">
+        <h3>
+          Download my resume <a href="/assets/files/resume.pdf">here</a>
+        </h3>
+        <br></br>
+        <h3>Front-end Proficiencies</h3>
+        <ul>{fronProf}</ul>
+        <h3>Back-end Proficiencies</h3>
+        <ul>{backProf}</ul>
+      </div>
+    </div>
+  );
 }
