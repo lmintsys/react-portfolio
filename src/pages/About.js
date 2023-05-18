6c05bc94ae277cdf4c8b82919adba829b687c1f7
lmintsys <117413193+lmintsys@users.noreply.github.com>
2023-05-17 21:02:59 -0400

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,7 +7,7 @@ export default function About() {
         <h2>About Me</h2>
       </div>
       <div className="container">
-        <img src="../../public/assets/images/profile.png" alt="profile" id="profile"></img>
+        <img src="./react-portfolio/assets/images/profile.png" alt="profile" id="profile"></img>
         <p className="text">
           Hello! My name is Luba Mintsys. I am coming from a psychology field,
           but have always been interested in programming. I have been working
